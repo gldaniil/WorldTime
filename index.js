080e78618c840002eec3c4fec9ccd13b82cf7278
gldaniil <daniil.gajfullin@yandex.ru>
2023-03-03 21:45:51 +0500

    Заменил условие тернарным оператором

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const myFunction = () => {
     let n = 0
     setInterval(function() {
         for (let i = 0; i < columnHeaders.length; i++) {
+            !columnHeaders[i].classList.contains('active') ? columnHeaders[i].classList.add('active') : 
             columnHeaders[i].innerHTML = `${n++} блок`
         }
     }, 1000)
