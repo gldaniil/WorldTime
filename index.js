7acb3d8692eed49862d225c3fc2ff95966c77814
gldaniil <daniil.gajfullin@yandex.ru>
2023-03-03 22:33:02 +0500

    New Version

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const myFunction = () => {
-    let columnHeaders = document.getElementsByClassName('content-time__header')
+    let columnTimes = document.getElementsByClassName('content-city__time')
     let n = 0
     setInterval(function() {
-        for (let i = 0; i < columnHeaders.length; i++) {
-            !columnHeaders[i].classList.contains('active') ? columnHeaders[i].classList.add('active') : 
-            columnHeaders[i].innerHTML = `${n++} блок`
+        for (let i = 0; i < columnTimes.length; i++) {
+            !columnTimes[i].classList.contains('active') ? columnTimes[i].classList.add('active') : columnTimes[i].classList.remove('active')
+            columnTimes[i].innerHTML = `${n++} блок`
         }
     }, 1000)
 }
